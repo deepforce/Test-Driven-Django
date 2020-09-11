311e2f95ab6e0a43aa39ceba043de64d75838e72
Zack <yuqz95@gmail.com>
2020-09-11 00:07:04 -0700

    new URL + view for adding to existing lists. FT passes

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -98,6 +98,6 @@ class NewVistorTest(LiveServerTestCase):
         # Again, there is no trace of Edith's list
         page_text = self.browser.find_element_by_tag_name('body').text
         self.assertNotIn('Buy peacock feathers', page_text)
-        self.assertIn('make a fly', page_text)
+        self.assertIn('Buy milk', page_text)
 
         # Satisfied, they both go back to sleep
