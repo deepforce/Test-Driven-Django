1daa7cf9a9da1ce5fddfc3fa85b993be9c8101bc
Zack <yuqz95@gmail.com>
2020-09-30 23:46:01 -0700

    Implement duplicate item validation at model layer

--- lists/tests/test_models.py
+++ lists/tests/test_models.py
@@ -33,8 +33,21 @@ class ItemModelTest(TestCase):
         list2 = List.objects.create()
         Item.objects.create(list=list1, text='bla')
         item = Item(list=list2, text='bla')
-        # item.full_clean() # Do not raise exception
-        item.save()
+        item.full_clean() # Do not raise exception
+
+    def test_list_ordering(self):
+        list1 = List.objects.create()
+        item1 = Item.objects.create(list=list1, text='i1')
+        item2 = Item.objects.create(list=list1, text='item 2')
+        item3 = Item.objects.create(list=list1, text='3')
+        self.assertEqual(
+            list(Item.objects.all()),
+            [item1, item2, item3]
+        )
+
+    def test_string_representation(self):
+        item = Item(text='some text')
+        self.assertEqual(str(item), 'some text')
 
 class ListModelTest(TestCase):
     def test_get_absolute_url(self):
