8778d2651742cc028eee64ee9eaa3feb6442bbed
Zack <yuqz95@gmail.com>
2020-10-05 02:15:07 -0700

    use uuid4 for default for our uid field

--- accounts/models.py
+++ accounts/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+import uuid
 
 # Create your models here.
 class User(models.Model):
@@ -7,3 +8,7 @@ class User(models.Model):
     USERNAME_FIELD = 'email'
     is_anonymous = False
     is_authenticated = True
+
+class Token(models.Model):
+    email = models.EmailField()
+    uid = models.CharField(default=uuid.uuid4, max_length=40)
